42b6816970728eba01ccb8c70e65fb74d97c0498
batuhan soylu <bthnsoylu35@gmail.com>
2023-11-17 21:59:39 +0300

    add pages and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { ThorinGlobalStyles, lightTheme, darkTheme } from "@ensdomains/thorin";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { WagmiConfig } from "wagmi";
+import { bscTestnet, scroll } from "wagmi/chains";
+
+const projectId = "149f5175dc4a4bc3d18eb9a1792c2ecc";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [bscTestnet, scroll];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={wagmiConfig}>
+      <ThemeProvider theme={lightTheme}>
+        <App />
+        <ThorinGlobalStyles />
+      </ThemeProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
