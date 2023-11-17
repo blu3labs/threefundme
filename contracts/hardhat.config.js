d62fd42365bef9e11d63bddf6cd547c34dde5284
CryptoS0s <devorso@users.noreply.github.com>
2023-11-17 22:02:41 +0300

    add hardhat network config

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+/** @type import('hardhat/config').HardhatUserConfig */
+const accountConfig = [process.env.PRIVATE_KEY]
+module.exports = {
+  solidity: "0.8.19",
+
+  networks: {
+    //testnet
+    baseGoerliTestnet:{
+      url:"https://base-goerli.gateway.tenderly.co",
+      accounts: accountConfig
+    },
+    scrollTestnet:{
+      url:"https://scroll-public.scroll-testnet.quiknode.pro",
+      accounts: accountConfig
+    },
+    zkSyncTestnet:{
+      url:"https://testnet.era.zksync.dev",
+      accounts: accountConfig
+    },
+    celoTestnet:{
+      url:"https://alfajores-forno.celo-testnet.org",
+      accounts: accountConfig
+    },
+    goerliTestnet:{
+      url:"https://rpc.ankr.com/eth_goerli",
+      accounts: accountConfig
+    },
+    sepoliaTestnet:{
+      url:"https://eth-sepolia.public.blastapi.io",
+      accounts: accountConfig
+    },
+    arbitrumTestnet:{
+      url:"wss://arbitrum-goerli.publicnode.com",
+      accounts: accountConfig
+    },
+    lineaTestnet:{
+      url:"https://rpc.goerli.linea.build",
+      accounts:accountConfig
+    }
+  }
+};
